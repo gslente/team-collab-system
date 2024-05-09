8b0b14321f872dad85ec9242187b6fda98c2503a
gslente <151698739+gslente@users.noreply.github.com>
2024-05-09 18:40:49 +0800

    TCS-18: Create accounts

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Account(models.Model):
+    user = models.ForeignKey('core.CustomUser', verbose_name=_('user'), on_delete=models.CASCADE)
