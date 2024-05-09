c69f5542f6c69d4bb25e6255064a9856e430f97b
gslente <151698739+gslente@users.noreply.github.com>
2024-05-09 18:42:24 +0800

    TCS-19: Add static URLs

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+STATIC_URLS = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + STATIC_URLS
